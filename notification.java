59cdcd389f01e8aff38511b93ca242ee325af003
Sflynn5 <38513810+Sflynn5@users.noreply.github.com>
2020-02-14 14:20:32 +0000

    Update notification.java

--- notification.java
+++ notification.java
@@ -4,6 +4,7 @@ import android.app.NotificationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -13,31 +14,40 @@ public class MainActivity extends AppCompatActivity {
     private static final String CHANNEL_ID = "channel";
     private static final String CHANNEL_NAME = "name";
     private static final String CHANNEL_DESCRIPTION = "description";
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            //Create channel instance
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            //set description
             channel.setDescription(CHANNEL_DESCRIPTION);
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
         }
 
+        //Click listener
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 displayNotification();
             }
         });
-        
+
     }
+
+    //Display the notification
     private void displayNotification() {
+        EditText total = (EditText)findViewById(R.id.Budget);
+        EditText spend = (EditText)findViewById(R.id.Spending);
+        int totalLeft = (Integer.parseInt(total.getText().toString())) - (Integer.parseInt(spend.getText().toString()));
+        //create builder instance
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground).setContentTitle("BUDGET LOW!")
-                .setContentText("You have less than $100 left in your monthly budget!")
+                .setSmallIcon(R.drawable.ic_launcher_foreground).setContentTitle("BUDGET UPDATE!")
+                .setContentText("You have $"+totalLeft+" left in your budget.")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         NotificationManagerCompat mNotificationMgr = NotificationManagerCompat.from(this);
         mNotificationMgr.notify(1, mBuilder.build());
